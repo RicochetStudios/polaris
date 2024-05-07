d8e14d477be7cab69e4764bf66110e71e33db49a
kiweezi <46600706+kiweezi@users.noreply.github.com>
2024-05-07 19:43:02 +0000

    Start working on functionality

--- internal/controller/gateway_controller.go
+++ internal/controller/gateway_controller.go
@@ -18,13 +18,20 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	polarisv1alpha1 "github.com/RicochetStudios/polaris/apis/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 // GatewayReconciler reconciles a Gateway object
@@ -47,11 +54,124 @@ type GatewayReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.2/pkg/reconcile
 func (r *GatewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	l := log.FromContext(ctx)
+	l.Info("starting reconcile")
+	defer l.Info("reconcile finished")
 
-	// TODO(user): your logic here
+	// Get the gateway.
+	gw := &polarisv1alpha1.Gateway{}
+	err := r.Get(ctx, req.NamespacedName, gw)
+	if apierrors.IsNotFound(err) {
+		l.Info("Gateway not found, assuming it was deleted")
+		return reconcile.Result{}, nil
+	} else if err != nil {
+		return reconcile.Result{}, fmt.Errorf("failed to get gateway: %w", err)
+	}
+
+	// Update the status of the gateway.
+	if err := r.setCurrentState(ctx, gw, l); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to set polaris state: %w", err)
+	}
+
+	// // Remove the finalizer and return before running any reconciles.
+	// // This is done to prevent reconcile functions from running when the gateway spec is empty.
+	// if gw.GetDeletionTimestamp() != nil {
+	// 	l.Info("Polaris is being deleted")
+	// 	if controllerutil.ContainsFinalizer(gw, gatewayFinalizer) {
+	// 		// // Run logic to perform before deleting the gateway.
+	// 		// if err := r.finalizeGateway(ctx, gw, l); err != nil {
+	// 		// 	return ctrl.Result{}, err
+	// 		// }
+
+	// 		l.Info("Gateway finalizer found, removing")
+	// 		controllerutil.RemoveFinalizer(gw, gatewayFinalizer)
+	// 		if err := r.Update(ctx, gw); err != nil {
+	// 			return ctrl.Result{}, err
+	// 		}
+	// 	}
+	// 	l.Info("Gateway resources cleaned up")
+	// 	return ctrl.Result{}, nil
+	// }
+
+	// if !controllerutil.ContainsFinalizer(gw, gatewayFinalizer) {
+	// 	// This log line is printed exactly once during initial provisioning,
+	// 	// because once the finalizer is in place this block gets skipped. So,
+	// 	// this is a nice place to tell the operator that the high level,
+	// 	// multi-reconcile operation is underway.
+	// 	l.Info("ensuring gateway is set up")
+	// 	gw.Status.Conditions = ""
+	// 	if err = r.Status().Update(ctx, gw); err != nil {
+	// 		return ctrl.Result{}, fmt.Errorf("failed to update gateway status: %w", err)
+	// 	}
+
+	// 	controllerutil.AddFinalizer(gw, gatewayFinalizer)
+	// 	if err := r.Update(ctx, gw); err != nil {
+	// 		return ctrl.Result{}, err
+	// 	}
+	// }
+
+	// Run logic to perform before setting up the gateway.
+	if err = r.maybeProvisionGateway(ctx, gw, l); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to privision gateway: %w", err)
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *GatewayReconciler) setCurrentState(ctx context.Context, gw *polarisv1alpha1.Gateway, l log.Logger) error {
+	// Get the child gateway.
+	childGw := &gatewayv1.Gateway{}
+	err := r.Get(ctx, types.NamespacedName{Name: gw.Name, Namespace: gw.Namespace}, childGw)
+	if err != nil {
+		return fmt.Errorf("failed to get gateway child resource: %w", err)
+	}
+
+	// Get the services assigned to the gateway.
+	_, err = r.getAssignedServices(ctx, gw, l)
+
+	// Set the status of the gateway.
+	gw.Status.Addresses = childGw.Status.Addresses
+	gw.Status.Conditions = childGw.Status.Conditions
+
+	if err := r.Status().Update(ctx, gw); err != nil {
+		return fmt.Errorf("failed to update gateway status: %w", err)
+	}
+
+	return nil
+}
+
+func (r *GatewayReconciler) getAssignedServices(ctx context.Context, gw *polarisv1alpha1.Gateway, l log.Logger) (corev1.Service, error) {
+	// // Get the services assigned to the gateway.
+	// services := &corev1.ServiceList{}
+	// if err := r.List(ctx, services, client.MatchingLabels(gw.Spec.Selector)); err != nil {
+	// 	return fmt.Errorf("failed to list services: %w", err)
+	// }
+
+	return corev1.Service{}, nil
+}
+
+func (r *GatewayReconciler) maybeProvisionGateway(ctx context.Context, gw *polarisv1alpha1.Gateway, l log.Logger) error {
+	// Get the child gateway.
+	childGw := &gatewayv1.Gateway{}
+	err := r.Get(ctx, types.NamespacedName{Name: gw.Name, Namespace: gw.Namespace}, childGw)
+	if err != nil {
+		return fmt.Errorf("failed to get gateway child resource: %w", err)
+	}
+
+	// Check if the gateway is already provisioned.
+	if childGw.Status.ObservedGeneration == gw.Generation {
+		l.Info("gateway already provisioned")
+		return nil
+	}
+
+	// Provision the gateway.
+	l.Info("provisioning gateway")
+	childGw.Spec = gw.Spec.GatewaySpec
+	if err := r.Update(ctx, childGw); err != nil {
+		return fmt.Errorf("failed to update gateway child resource: %w", err)
+	}
+
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
